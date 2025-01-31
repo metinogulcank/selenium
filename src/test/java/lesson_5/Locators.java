2ebaf9e95ff2d832a8cd569b59c6e7de648bc0e9
metinogulcank <metinogulcank06@gmail.com>
2025-01-31 03:57:39 +0300

    Locators

--- src/test/java/lesson_5/Locators.java
+++ src/test/java/lesson_5/Locators.java
@@ -32,6 +32,55 @@ public class Locators {
 
         WebElement searchBoxLinkElement2 = driver.findElement(By.linkText("Forgotton Password?"));
 
+        By searchBoxPartialLink = By.partialLinkText("Forgotten Pass"); // linkTextten farklı olarak sadece bir kısmını yazmamız yeterlidir.
+        WebElement searchBoxPartialLinkElement = driver.findElement(searchBoxPartialLink);
+        WebElement searchBoxPartialLinkElement2 = driver.findElement(By.partialLinkText("Forgotten Pass");
+
+        By searchBoxTagName = By.tagName("input"); // Tag'ı input olan değerleri seçer. Birden çok değer döndürür.
+        WebElement searchBoxTagNameElement = driver.findElement(searchBoxTagName);
+        WebElement searchBoxTagNameElement2 = driver.findElement(By.tagName("input"));
+
+        // Absolute xPath
+
+        // // div / table / tbody
+
+        // /html/body/ytd-app/div[1]/div/ytd-masthead/div[3]/div[2]/ytd-searchbox/form/div[1]/div[1]/input
+
+        // Sadece tagname kullanarak
+        driver.findElement(By.xpath("//input"));
+
+        // Tagname farketmeksizin attribute name ve attribute value kullanarak
+        driver.findElement(By.xpath("// * [@type='text']"));
+
+        // Atribute name farketmeksizin tagname ve attribute value kullanarak
+        driver.findElement(By.xpath("// input[@ *='text']"));
+
+        // Atribute value farketmeksizin tagname ve attribute name kullanarak
+        driver.findElement(By.xpath("// input[@type]"));
+
+        // Bazen de attribute bağlı olmadan sadece text ile bulunabilir
+
+        // Belirli bir text ile element bulma
+        driver.findElement(By.xpath("//tagname[.='text name']"));
+        driver.findElement(By.xpath("//tagname[.='text name']"));
+
+        // Belli bir metni içeren bir öğeyi bulmak için
+        driver.findElement(By.xpath("//*[contains(text(),'Bir Parça Yazı')]"));
+
+        // Tek bir attribute ile unique sonuca ulaşılmazsa birden fazla attribute yazılabilir.
+
+        driver.findElement(By.xpath("//div[@id='logo' or class = 'flex-col logo']"));
+        driver.findElement(By.xpath("//div[@id='logo' and class = 'flex-col logo']"));
+
+
+
+        // Css selector xpath'e benzer. 3 kullanımı var
+        // TagName[attribute name='value']
+        driver.findElement(By.cssSelector("input[name='session[password]']"));
+        // tagname#idValue veya #idvalue
+        driver.findElement(By.cssSelector("input#session_password"));
+        // tagname.classValue veya sadece .classValue
+        driver.findElement(By.cssSelector(".form-control"));
 
     }
 }
